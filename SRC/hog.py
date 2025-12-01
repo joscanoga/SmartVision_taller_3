fe37c4ce3c2ce3e79a714889629df38dddb1cafc
joscanoga <51725572+joscanoga@users.noreply.github.com>
2025-11-30 20:29:56 -0500

    descriptor hog

---
+++ SRC/hog.py
@@ -0,0 +1,25 @@
+#clase para manejar histogramas de gradientes orientados (HOG)import cv2, y sus funcionalidades básicas, y graficos
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+
+class HOG:
+    def __init__(self, win_size=(64, 128), block_size=(16, 16), block_stride=(8, 8),
+                 cell_size=(8, 8), nbins=9):
+        self.hog = cv2.HOGDescriptor(win_size, block_size, block_stride, cell_size, nbins)
+
+    def compute(self, image):
+        """
+        Calcula el descriptor HOG para una imagen dada.
+
+        :param image: Imagen de entrada en escala de grises.
+        :return: Descriptor HOG como un array numpy.
+        """
+        if len(image.shape) != 2:
+            # Asegurarse de que la imagen esté en escala de grises
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        
+        hog_descriptor = self.hog.compute(image)
+        return hog_descriptor.flatten()
+    
+    
